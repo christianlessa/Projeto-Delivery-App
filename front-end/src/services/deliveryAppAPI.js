a256e2105603359f67c6c085830695feccd312ff
fshinkae <felipeshinkae97@gmail.com>
2022-08-06 22:03:30 -0400

    Feat: Created service front-end

---
+++ front-end/src/services/deliveryAppAPI.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default async function deliveryAppAPI(input) {
+  switch (input) {
+  // case para o usuário logar
+  case 'loginUser': {
+    const { token } = JSON.parse(localStorage.getItem('user'));
+    const loginUser = await axios({
+      url: 'http://localhost:3001/login/',
+      data: bodyValue,
+      headers: { authorization: token },
+    });
+    return loginUser.data;
+  }
+  default:
+    return console.log('error');
+  }
+}
