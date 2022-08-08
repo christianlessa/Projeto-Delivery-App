393d02b65a9e1fb7d2108ce1ea921db6e09e174b
fshinkae <felipeshinkae97@gmail.com>
2022-08-08 17:43:52 -0400

    Feat: added to backend, images logic

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const loginRouter = require('./routes/routesLogin');
 const registerRouter = require('./routes/routesRegister');
 const productRouter = require('./routes/routesProducts');
+
+// https://expressjs.com/pt-br/starter/static-files.html
+
+const imagesUpload = path.join(__dirname, '..', '..', '..', 'public', 'images');
 
 const app = express();
 
@@ -13,6 +18,7 @@ app.use(cors());
 app.use('/login', loginRouter);
 app.use('/register', registerRouter);
 app.use('/customer/products', productRouter);
+app.use('/images', express.static(imagesUpload));
 
 // app.get('/coffee', (_req, res) => res.status(418).end());
 
